2372bbadb01ec052e3fc4c3f894a537425aafaa9
Daniel Homola <dani.homola@gmail.com>
2017-04-30 18:09:21 +0100

    adding favicon

--- frontend/views.py
+++ frontend/views.py
@@ -390,5 +390,6 @@ def all_exception_error(exception):
 
 @app.route('/robots.txt')
 @app.route('/sitemap.xml')
+@app.route('/favicon.ico')
 def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
